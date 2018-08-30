f0624e4481f84b534ddf8935364b7c7082b71d79
liyan <yanx0167@stud.kea.dk>
2018-08-30 16:48:38 +0200

    functions finished, optional work (styling) not done

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,9 +1,19 @@
 "use strict";
+const detailButtons = document.querySelectorAll("a");
+const modal = document.querySelector("#modal_container");
 
+detailButtons.forEach(detailButtonClicked);
+function detailButtonClicked(b) {
+  b.addEventListener("click", showModal);
+}
 function showModal() {
   console.log("Show modal");
+  modal.style.display = "inherit";
 }
+const closeX = document.querySelector("#modal_container span");
+closeX.addEventListener("click", hideModal);
 
 function hideModal() {
   console.log("Hide modal");
+  modal.style.display = "none";
 }
