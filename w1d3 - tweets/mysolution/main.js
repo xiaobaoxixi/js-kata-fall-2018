77c199cda1b5b32d37cb3812d644bd98925e375f
liyan <yanx0167@stud.kea.dk>
2018-08-30 18:45:06 +0200

    hashtags collected and duplicates removed

--- w1d3 - tweets/mysolution/main.js
+++ w1d3 - tweets/mysolution/main.js
@@ -5,11 +5,27 @@ fetch("https://kea-alt-del.dk/twitter/api/")
   .then(showTweets);
 function showTweets(data) {
   let fetchedTweets = data.statuses;
+  let hashtagsArray = [];
   fetchedTweets.forEach(singleTweet);
   function singleTweet(t) {
     let clone = template.cloneNode(true);
     clone.querySelector("p").textContent = t.text;
+    if (t.entities.hashtags.length > 0) {
+      console.log("has");
+      let allHashtags = t.entities.hashtags;
+      allHashtags.forEach(getHashtag);
+      function getHashtag(h) {
+        if (hashtagsArray.indexOf(h.text) < 0) {
+          hashtagsArray.push(h.text); // remove duplicates
+        }
+      }
+    }
     document.querySelector("#tweets-section").appendChild(clone);
   }
-  console.log(fetchedTweets.length);
+  hashtagsArray.forEach(collectHashtag);
+  function collectHashtag(h) {
+    let hashtagSpan = document.createElement("span");
+    hashtagSpan.textContent = `#${h} `;
+    document.querySelector("#hashtags").appendChild(hashtagSpan);
+  }
 }
