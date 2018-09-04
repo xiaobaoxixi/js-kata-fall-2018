fabca6a317050e08587d6e711f824a75a7db7992
liyan <yanx0167@stud.kea.dk>
2018-09-04 18:45:53 +0200

    basic kata finished

--- w2d2 - findAll/mysolution/find-all.js
+++ w2d2 - findAll/mysolution/find-all.js
@@ -1,1 +1,15 @@
 "use strict";
+/** basic kata */
+let foundIndex = [];
+function findAll(arr, search) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === search) {
+      foundIndex.push(i);
+    }
+  }
+  console.log(foundIndex);
+}
+findAll(
+  ["dog", "cat", "horse", "cat", "mouse", "dog", "cat", "cat", "dog"],
+  "cat"
+);
