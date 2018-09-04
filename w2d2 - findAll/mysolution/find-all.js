276307e58b77248d7411303fb1dacade231e31b3
liyan <yanx0167@stud.kea.dk>
2018-09-04 19:00:26 +0200

    optional kata finished

--- w2d2 - findAll/mysolution/find-all.js
+++ w2d2 - findAll/mysolution/find-all.js
@@ -1,5 +1,6 @@
 "use strict";
 /** basic kata */
+let arr = ["dog", "cat", "horse", "cat", "mouse", "dog", "cat", "cat", "dog"];
 let foundIndex = [];
 function findAll(arr, search) {
   for (let i = 0; i < arr.length; i++) {
@@ -9,7 +10,13 @@ function findAll(arr, search) {
   }
   console.log(foundIndex);
 }
-findAll(
-  ["dog", "cat", "horse", "cat", "mouse", "dog", "cat", "cat", "dog"],
-  "cat"
-);
+findAll(arr, "cat");
+
+/** optional kata */
+let foundIndex2 = arr.map(function(item, index) {
+  if (item === "cat") {
+    return index;
+  }
+});
+let foundIndex2ClearedUp = foundIndex2.filter(item => item > -1); //clear out the falses in the []
+console.log(foundIndex2ClearedUp);
