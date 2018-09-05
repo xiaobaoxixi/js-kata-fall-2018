e276055122370ed25f7093373e3b2f9a73ddf34e
liyan <yanx0167@stud.kea.dk>
2018-09-05 15:59:22 +0200

    added: buttons and functions for generate same hue/saturation/lightness

--- w4d1 - Color Scheme Generator/mysolution/main.js
+++ w4d1 - Color Scheme Generator/mysolution/main.js
@@ -33,3 +33,50 @@ function generateHSL(d) {
   d.textContent = `hsl(${hslColors})`;
   d.style.backgroundColor = `hsl(${hslColors})`;
 }
+/** optional: same hue */
+document.querySelector(".hue").addEventListener("click", generateSameHue);
+function generateSameHue() {
+  let hslColors = [];
+  let hue = Math.floor(Math.random() * 361);
+  hslColors.push(hue);
+  divHSLArray.forEach(assignSameHue);
+  function assignSameHue(d) {
+    hslColors[1] = `${Math.floor(Math.random() * 101)}%`;
+    hslColors[2] = `${Math.floor(Math.random() * 101)}%`;
+    d.textContent = `hsl(${hslColors})`;
+    d.style.backgroundColor = `hsl(${hslColors})`;
+  }
+}
+/** optional: same saturation */
+document
+  .querySelector(".saturation")
+  .addEventListener("click", generateSameSaturation);
+function generateSameSaturation() {
+  let hslColors = [];
+  let saturation = Math.floor(Math.random() * 101);
+  divHSLArray.forEach(assignSameHue);
+  function assignSameHue(d) {
+    hslColors[0] = `${Math.floor(Math.random() * 361)}`;
+    hslColors[1] = `${saturation}%`;
+    hslColors[2] = `${Math.floor(Math.random() * 101)}%`;
+    d.textContent = `hsl(${hslColors})`;
+    d.style.backgroundColor = `hsl(${hslColors})`;
+  }
+}
+
+/** optional: same lightness */
+document
+  .querySelector(".lightness")
+  .addEventListener("click", generateSameLightness);
+function generateSameLightness() {
+  let hslColors = [];
+  let lightness = Math.floor(Math.random() * 101);
+  divHSLArray.forEach(assignSameHue);
+  function assignSameHue(d) {
+    hslColors[0] = `${Math.floor(Math.random() * 361)}`;
+    hslColors[1] = `${Math.floor(Math.random() * 101)}%`;
+    hslColors[2] = `${lightness}%`;
+    d.textContent = `hsl(${hslColors})`;
+    d.style.backgroundColor = `hsl(${hslColors})`;
+  }
+}
