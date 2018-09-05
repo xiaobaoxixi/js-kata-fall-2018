58b4e98bf4acb822143a32863755e9cc0d98f8d8
liyan <yanx0167@stud.kea.dk>
2018-09-05 17:22:52 +0200

    finished: change to random color and not the same color as previous

--- w4d2 - background-transition/mysolution/main.js
+++ w4d2 - background-transition/mysolution/main.js
@@ -1,1 +1,18 @@
 "use strict";
+let hsl;
+let hslArray = [];
+window.addEventListener("load", generateHSL);
+function generateHSL() {
+  let hue = Math.floor(Math.random() * 361);
+  let s = Math.floor(Math.random() * 100) + "%";
+
+  let l = Math.floor(Math.random() * 100) + "%";
+  hsl = `hsl(${hue}, ${s}, ${l})`;
+  if (hsl !== hslArray[hslArray.length - 1]) {
+    document.querySelector("div").style.backgroundColor = hsl;
+    hslArray.push(hsl);
+    setTimeout(generateHSL, 2000);
+  } else {
+    generateHSL();
+  }
+}
