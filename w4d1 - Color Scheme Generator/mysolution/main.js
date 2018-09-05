b51bae367330e62b0bc66ffbf2ca692683d42903
liyan <yanx0167@stud.kea.dk>
2018-09-05 15:45:24 +0200

    finished: optional 1: no duplicate rgb color

--- w4d1 - Color Scheme Generator/mysolution/main.js
+++ w4d1 - Color Scheme Generator/mysolution/main.js
@@ -2,19 +2,29 @@
 
 const divRGBArray = document.querySelectorAll(".rgb");
 const divHSLArray = document.querySelectorAll(".hsl");
+let rgbColors = [];
+let rgbCodes;
+let rgb;
 
 divRGBArray.forEach(generateRGB);
 divHSLArray.forEach(generateHSL);
+
 function generateRGB(d) {
-  let rgbColors = [];
-  let rgb;
+  rgbCodes = [];
   for (let i = 0; i < 3; i++) {
     rgb = Math.floor(Math.random() * 256);
-    rgbColors.push(rgb);
+    rgbCodes.push(rgb);
   }
-  d.textContent = `rgb(${rgbColors})`;
-  d.style.backgroundColor = `rgb( ${rgbColors})`;
+  /** optional 1: no duplicated colors */
+  if (rgbColors.indexOf(rgbCodes) < 0) {
+    rgbColors.push(rgbCodes);
+    d.textContent = `rgb(${rgbCodes})`;
+    d.style.backgroundColor = `rgb( ${rgbCodes})`;
+  } else {
+    generateRGB();
+  }
 }
+
 function generateHSL(d) {
   let hslColors = [];
   hslColors.push(Math.floor(Math.random() * 361));
