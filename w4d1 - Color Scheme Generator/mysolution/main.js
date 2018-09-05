7a3a0ea9aa795c0aad0abaa44502e226d49d29a8
liyan <yanx0167@stud.kea.dk>
2018-09-05 15:34:54 +0200

    added: divs with hsl color automaticly assigned

--- w4d1 - Color Scheme Generator/mysolution/main.js
+++ w4d1 - Color Scheme Generator/mysolution/main.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const divArray = document.querySelectorAll("div");
-divArray.forEach(generateRGB);
+const divRGBArray = document.querySelectorAll(".rgb");
+const divHSLArray = document.querySelectorAll(".hsl");
+
+divRGBArray.forEach(generateRGB);
+divHSLArray.forEach(generateHSL);
 function generateRGB(d) {
   let rgbColors = [];
   let rgb;
@@ -11,4 +14,12 @@ function generateRGB(d) {
   }
   d.textContent = `rgb(${rgbColors})`;
   d.style.backgroundColor = `rgb( ${rgbColors})`;
+}
+function generateHSL(d) {
+  let hslColors = [];
+  hslColors.push(Math.floor(Math.random() * 361));
+  hslColors.push(`${Math.floor(Math.random() * 101)}%`);
+  hslColors.push(`${Math.floor(Math.random() * 101)}%`);
+  d.textContent = `hsl(${hslColors})`;
+  d.style.backgroundColor = `hsl(${hslColors})`;
 }
