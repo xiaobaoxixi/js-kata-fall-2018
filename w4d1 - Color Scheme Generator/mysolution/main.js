62b1c49fba4a6b4dd7497e11a7cd36c0792a8548
liyan <yanx0167@stud.kea.dk>
2018-09-05 17:06:22 +0200

    added: optional: add hsl stripe + if already same hue, then add only color of this hue. NOT OPTIMIZED: remove newly added stripes with clicking not working yet. combination of random and same h/s/l not ideal. no indication for same h/s/l yet

--- w4d1 - Color Scheme Generator/mysolution/main.js
+++ w4d1 - Color Scheme Generator/mysolution/main.js
@@ -1,10 +1,15 @@
 "use strict";
 
-const divRGBArray = document.querySelectorAll(".rgb");
-const divHSLArray = document.querySelectorAll(".hsl");
+const divRGBArray = document.querySelectorAll("div.rgb");
+const divHSLArray = document.querySelectorAll("div.hsl");
 let rgbColors = [];
+let hslColors = [];
 let rgbCodes;
 let rgb;
+let randomHSL = true;
+let sameHue = false;
+let sameSaturation = false;
+let sameLightness = false;
 
 divRGBArray.forEach(generateRGB);
 divHSLArray.forEach(generateHSL);
@@ -26,25 +31,28 @@ function generateRGB(d) {
 }
 
 function generateHSL(d) {
-  let hslColors = [];
-  hslColors.push(Math.floor(Math.random() * 361));
-  hslColors.push(`${Math.floor(Math.random() * 101)}%`);
-  hslColors.push(`${Math.floor(Math.random() * 101)}%`);
-  d.textContent = `hsl(${hslColors})`;
-  d.style.backgroundColor = `hsl(${hslColors})`;
+  let hslCodes = [];
+  hslCodes.push(Math.floor(Math.random() * 361));
+  hslCodes.push(`${Math.floor(Math.random() * 101)}%`);
+  hslCodes.push(`${Math.floor(Math.random() * 101)}%`);
+  hslColors.push(hslCodes);
+  if (d) d.textContent = `hsl(${hslCodes})`;
+  if (d) d.style.backgroundColor = `hsl(${hslCodes})`;
 }
 /** optional: same hue */
 document.querySelector(".hue").addEventListener("click", generateSameHue);
 function generateSameHue() {
-  let hslColors = [];
+  randomHSL = false;
+  sameHue = true;
+  let hslCodes = [];
   let hue = Math.floor(Math.random() * 361);
-  hslColors.push(hue);
+  hslCodes.push(hue);
   divHSLArray.forEach(assignSameHue);
   function assignSameHue(d) {
-    hslColors[1] = `${Math.floor(Math.random() * 101)}%`;
-    hslColors[2] = `${Math.floor(Math.random() * 101)}%`;
-    d.textContent = `hsl(${hslColors})`;
-    d.style.backgroundColor = `hsl(${hslColors})`;
+    hslCodes[1] = `${Math.floor(Math.random() * 101)}%`;
+    hslCodes[2] = `${Math.floor(Math.random() * 101)}%`;
+    d.textContent = `hsl(${hslCodes})`;
+    d.style.backgroundColor = `hsl(${hslCodes})`;
   }
 }
 /** optional: same saturation */
@@ -52,15 +60,17 @@ document
   .querySelector(".saturation")
   .addEventListener("click", generateSameSaturation);
 function generateSameSaturation() {
-  let hslColors = [];
+  randomHSL = false;
+  sameSaturation = true;
+  let hslCodes = [];
   let saturation = Math.floor(Math.random() * 101);
   divHSLArray.forEach(assignSameHue);
   function assignSameHue(d) {
-    hslColors[0] = `${Math.floor(Math.random() * 361)}`;
-    hslColors[1] = `${saturation}%`;
-    hslColors[2] = `${Math.floor(Math.random() * 101)}%`;
-    d.textContent = `hsl(${hslColors})`;
-    d.style.backgroundColor = `hsl(${hslColors})`;
+    hslCodes[0] = `${Math.floor(Math.random() * 361)}`;
+    hslCodes[1] = `${saturation}%`;
+    hslCodes[2] = `${Math.floor(Math.random() * 101)}%`;
+    d.textContent = `hsl(${hslCodes})`;
+    d.style.backgroundColor = `hsl(${hslCodes})`;
   }
 }
 
@@ -69,15 +79,17 @@ document
   .querySelector(".lightness")
   .addEventListener("click", generateSameLightness);
 function generateSameLightness() {
-  let hslColors = [];
+  randomHSL = false;
+  sameLightness = true;
+  let hslCodes = [];
   let lightness = Math.floor(Math.random() * 101);
   divHSLArray.forEach(assignSameHue);
   function assignSameHue(d) {
-    hslColors[0] = `${Math.floor(Math.random() * 361)}`;
-    hslColors[1] = `${Math.floor(Math.random() * 101)}%`;
-    hslColors[2] = `${lightness}%`;
-    d.textContent = `hsl(${hslColors})`;
-    d.style.backgroundColor = `hsl(${hslColors})`;
+    hslCodes[0] = `${Math.floor(Math.random() * 361)}`;
+    hslCodes[1] = `${Math.floor(Math.random() * 101)}%`;
+    hslCodes[2] = `${lightness}%`;
+    d.textContent = `hsl(${hslCodes})`;
+    d.style.backgroundColor = `hsl(${hslCodes})`;
   }
 }
 
@@ -94,8 +106,36 @@ function removeDiv(d) {
 document.querySelector(".addRGB").addEventListener("click", addRGB);
 function addRGB() {
   let newDiv = document.createElement("div");
+  newDiv.classList.add("rgb");
   generateRGB();
   newDiv.style.backgroundColor = `rgb(${rgbColors[rgbColors.length - 1]})`;
   newDiv.textContent = `rgb(${rgbColors[rgbColors.length - 1]})`;
-  document.querySelector("section").appendChild(newDiv);
+  document.querySelector("section.rgb").appendChild(newDiv);
 }
+
+/** optional add hsl color stripe */
+document.querySelector(".addHSL").addEventListener("click", addHSL);
+function addHSL() {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("hsl");
+  if (randomHSL) {
+    generateHSL();
+    newDiv.style.backgroundColor = `hsl(${hslColors[hslColors.length - 1]})`;
+    newDiv.textContent = `hsl(${hslColors[hslColors.length - 1]})`;
+    document.querySelector("section.hsl").appendChild(newDiv);
+  } else if (sameHue) {
+    let currentHSLValues = document
+      .querySelector("div.hsl")
+      .textContent.split(",");
+    let hue = currentHSLValues[0].slice(currentHSLValues[0].indexOf("(") + 1);
+    let hslCodes = [];
+    hslCodes.push(hue);
+    hslCodes[1] = `${Math.floor(Math.random() * 101)}%`;
+    hslCodes[2] = `${Math.floor(Math.random() * 101)}%`;
+    newDiv.textContent = `hsl(${hslCodes})`;
+    newDiv.style.backgroundColor = `hsl(${hslCodes})`;
+    document.querySelector("section.hsl").appendChild(newDiv);
+  }
+}
+
+/** refresh all with button */
