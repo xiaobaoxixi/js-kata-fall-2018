4629e6c79bd93f32874942920fc4e0cb117b0455
liyan <yanx0167@stud.kea.dk>
2018-09-05 15:08:13 +0200

    finished. done after load, no trigger added

--- w2d3 - delete the bad review/mysolution/main.js
+++ w2d3 - delete the bad review/mysolution/main.js
@@ -1,1 +1,10 @@
 "use strict";
+const ratingsArray = document.querySelectorAll("ul li span");
+window.addEventListener("load", ratingsArray.forEach(removeLowRating));
+console.log(ratingsArray);
+
+function removeLowRating(r) {
+  if (r.textContent <= 5) {
+    r.parentElement.remove();
+  }
+}
