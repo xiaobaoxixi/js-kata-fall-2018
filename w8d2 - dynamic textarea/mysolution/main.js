a6445c083a89aa9f80bf7d0598bb91e60d2265d6
liyan <yanx0167@stud.kea.dk>
2018-09-05 19:37:52 +0200

    finished including optional

--- w8d2 - dynamic textarea/mysolution/main.js
+++ w8d2 - dynamic textarea/mysolution/main.js
@@ -1,1 +1,16 @@
 "use strict";
+window.addEventListener("keydown", checkTextCount);
+function checkTextCount() {
+  const textarea = document.querySelector("textarea");
+  let textCount = textarea.value.length; // use value instead of textContent
+  console.log(textCount);
+  if (textCount < 20) {
+    textarea.style.fontSize = "37px";
+  } else if (textCount < 30) {
+    textarea.style.fontSize = "31px";
+  } else if (textCount < 70) {
+    textarea.style.fontSize = "23px";
+  } else {
+    textarea.style.fontSize = "17px";
+  }
+}
