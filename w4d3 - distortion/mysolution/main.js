8880f8c6b8fc18005ceef3e73dca989c86bc3e3b
liyan <yanx0167@stud.kea.dk>
2018-09-18 15:05:29 +0200

    cleared up js

--- w4d3 - distortion/mysolution/main.js
+++ w4d3 - distortion/mysolution/main.js
@@ -6,21 +6,13 @@ function update() {
     .then(d => newNr(d));
   function newNr(n) {
     nr = Number(n.inQueue);
-    let s = 0;
     changeSize();
     function changeSize() {
-      if (s < 10) {
-        document.querySelector(".nrInQueue").style.width = `${10 + nr * 1.5}vh`;
-        document.querySelector(".nrInQueue").style.height = `${10 +
-          nr * 1.5}vh`;
-        document.querySelector(
-          ".nrInQueue"
-        ).style.margin = `calc(50vh - .5*${10 + nr * 1.5}vh) auto`;
-      }
-      s++;
-      setTimeout(changeSize, 100);
+      document.querySelector(".nrInQueue").style.width = `${10 + nr * 1.5}vh`;
+      document.querySelector(".nrInQueue").style.height = `${10 + nr * 1.5}vh`;
+      document.querySelector(".nrInQueue").style.margin = `calc(50vh - .5*${10 +
+        nr * 1.5}vh) auto`;
     }
-    console.log(`${nr} ${10 + nr * 1.5}`);
   }
   setTimeout(update, 10000);
 }
